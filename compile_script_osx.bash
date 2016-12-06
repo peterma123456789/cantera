445f954733e7169be647c43e950e8f14c3147b8d
wuhao <wuhao@stanford.edu>
2016-12-05 20:36:08 -0800

    nothing

--- compile_script_osx.bash
+++ compile_script_osx.bash
@@ -3,11 +3,11 @@
 export CANTERA_DIR=/Users/wuhao/Codes/Cantera_Chemistry/Cantera/2.3.0
 export SUN_INCLUDE=/Users/wuhao/Codes/tools/sundials/2.6.2/include
 export SUN_LIB=/Users/wuhao/Codes/tools/sundials/2.6.2/lib
-export BOOST_DIR=/usr/local/Cellar/boost/1.59.0/include
-export EIGEN_INCLUDE=/usr/local/Cellar/eigen/3.2.6/include/eigen3
+export BOOST_DIR=/usr/local/Cellar/boost/1.62.0/include
+export EIGEN_INCLUDE=/usr/local/Cellar/eigen/3.2.8/include/eigen3
 
-scons build prefix=$CANTERA_DIR CXX=clang++ CC=clang FORTRAN=gfortran-5 python_package=full \
+scons -j5 build prefix=$CANTERA_DIR CXX=clang++ CC=clang python_package=full \
     sundials_include=$SUN_INCLUDE sundials_libdir=$SUN_LIB matlab_path=/Applications/MATLAB_R2014b.app/ \
-    boost_inc_dir=$BOOST_DIR f90_interface=y eigen_include=$EIGEN_INCLUDE
+    boost_inc_dir=$BOOST_DIR f90_interface=n eigen_include=$EIGEN_INCLUDE
 # scons test
-scons install
+scons -j5 install
