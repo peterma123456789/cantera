60fda18b679154ee66051f4972bdcfd40e4ae1bf
wuhao <wuhao@stanford.edu>
2017-02-02 16:21:41 -0800

    modified interface for RxnActivMgr

--- include/cantera/kinetics/RxnActivMgr.h
+++ include/cantera/kinetics/RxnActivMgr.h
@@ -13,42 +13,43 @@
 
 #include "Kinetics.h"
 
-namespace Cantera
-{
-class RxnActivMgr
-{
+namespace Cantera {
+class RxnActivMgr {
 public:
-
-  typedef Eigen::SparseMatrix<double,Eigen::ColMajor> SpCMat;
-  typedef Eigen::SparseMatrix<double,Eigen::RowMajor> SpRMat;
+  typedef Eigen::SparseMatrix<double, Eigen::ColMajor> SpCMat;
+  typedef Eigen::SparseMatrix<double, Eigen::RowMajor> SpRMat;
   typedef Eigen::VectorXd VecType;
 
-  RxnActivMgr(Kinetics* _kin) : m_kinetics(_kin), m_nSpecs(0), m_nRxns(0) {}
-
-  Kinetics& kinetics() {
-    return *m_kinetics;
+  RxnActivMgr(Kinetics *_kin)
+      : m_kinetics(_kin), m_nSpecs(0), m_nRxns(0), m_nactiv(0) {
+    updateStoichMatrix();
   }
 
-  const Kinetics& kinetics() const {
-    return *m_kinetics;
-  }
+  Kinetics &kinetics() { return *m_kinetics; }
+
+  const Kinetics &kinetics() const { return *m_kinetics; }
 
   // Update the stoich. matrix from the Kinetics
   void updateStoichMatrix();
 
-  // Choose active reactions
+  // Choose activ reactions
   void updateActivRxns(const double relTol, const double absTol);
 
-  // get m_iactive
-  const std::vector<std::uint8_t>& iActiv() { return m_iactive; }
+  // get m_iactiv
+  const std::vector<std::uint8_t> &getActiv() const { return m_iactiv; }
+
+  // get numer of activ reactions
+  size_t getNumActiv() const { return m_nactiv; }
+
 protected:
   //! pointer to the Kinetics object
-  Kinetics* m_kinetics;
+  Kinetics *m_kinetics;
   size_t m_nSpecs;
   size_t m_nRxns;
 
   //! Record of reaction activation
-  std::vector<std::uint8_t> m_iactive;
+  std::vector<std::uint8_t> m_iactiv;
+  std::size_t m_nactiv;
 
   //! Sparse matrices for adaptive chemistry
   //! Stoich. matrix
@@ -61,8 +62,11 @@ protected:
   //! Workign array of same size as stoich. matrix
   SpCMat m_wm;
 
-  // ! Resize matrices and vectors
+  //! Resize matrices and vectors
   void resizeData(const size_t _nSpecs, const size_t _nRxns);
+
+  //! Calculate m_nactiv
+  size_t updateNumActiv(const std::vector<std::uint8_t> &_iactiv);
 };
 }
 
