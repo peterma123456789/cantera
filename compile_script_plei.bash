e19ab17ab353fc2f85d9f18e25159b4ccd4dddbd
peterma <peterma123456789@gmail.com>
2017-03-02 18:27:11 -0800

    compile_script_plei.bash

--- compile_script_plei.bash
+++ compile_script_plei.bash
@@ -1,18 +1,18 @@
 #! /bin/bash
 # Need to build SUNDIALS with -fPIC and set(CMAKE_MACOSX_RPATH 1)
-export CANTERA_DIR=/u/hwu8/Code/Cantera_CharlesX/2.3.0
-export SUN_INCLUDE=/u/hwu8/Library/sundials/2.6.2/include
-export SUN_LIB=/u/hwu8/Library/sundials/2.6.2/lib
-export BOOST_DIR=/nasa/boost/1.50.0//include
-export EIGEN_INCLUDE=/u/hwu8/Library/eigen/3.3-rc1/
+export CANTERA_DIR=/nobackup/cma3/local/cantera/2.4.0
+export SUN_INCLUDE=/nobackup/cma3/local/sundials/2.7.0/include
+export SUN_LIB=/nobackup/cma3/local/sundials/2.7.0/lib
+export BOOST_DIR=/nobackup/cma3/local/boost/1.63.0/include
+export EIGEN_INCLUDE=/nobackup/cma3/local/eigen/3.3.2/eigen/
 
 scons -j20 build prefix=$CANTERA_DIR \
   CXX=icpc CC=icc FORTRAN=ifort python_package=full \
   optimize_flags='-O3 -ip -axCORE-AVX2 -xAVX' \
   env_vars='all' \
   sundials_include=$SUN_INCLUDE sundials_libdir=$SUN_LIB \
-  boost_inc_dir=$BOOST_DIR f90_interface=y eigen_include=$EIGEN_INCLUDE
+  boost_inc_dir=$BOOST_DIR f90_interface=y system_eigen=y extra_inc_dirs=$EIGEN_INCLUDE
 if [ -z "SCONS_TEST" ]; then
     scons -j20 test
-fi  
+fi
 scons -j20 install
