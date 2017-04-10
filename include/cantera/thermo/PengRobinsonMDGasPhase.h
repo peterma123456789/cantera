3a5dfbd7888652879ce46f4f7dab1ed3ef12cb88
peterma <peterma123456789@gmail.com>
2017-04-10 11:41:22 -0700

    linear interpolation implemented for MD; lots of hard-coding for the single-species (AR) test case

--- include/cantera/thermo/PengRobinsonMDGasPhase.h
+++ include/cantera/thermo/PengRobinsonMDGasPhase.h
@@ -26,13 +26,34 @@ class PengRobinsonMDGasPhase: public PengRobinsonGasPhase
 {
 public:
     //! Default empty Constructor
-    PengRobinsonMDGasPhase() : PengRobinsonGasPhase() {};
+    PengRobinsonMDGasPhase() : PengRobinsonGasPhase()
+    {
+        std::vector<double> T_tmp, rho_tmp, cp_tmp;
+        T_tmp.push_back(10.0);
+        T_tmp.push_back(5000.0);
+        rho_tmp.push_back(1000.0);
+        rho_tmp.push_back(10.0);
+        cp_tmp.push_back(4.0e4);
+        cp_tmp.push_back(4.0e4);
+        mySplineRho.set_points(T_tmp, rho_tmp);
+        mySplineCp.set_points(T_tmp, cp_tmp);
+
+        myLinearT.push_back(10.0);
+        myLinearT.push_back(5000.0);
+        myLinearRho.push_back(1000.0);
+        myLinearRho.push_back(10.0);
+        myLinearCp.push_back(4.0e4);
+        myLinearCp.push_back(4.0e4);
+    };
 
     //! Copy Constructor
     PengRobinsonMDGasPhase(const PengRobinsonMDGasPhase& right)
         : PengRobinsonGasPhase(right),
           mySplineRho(right.mySplineRho),
-          mySplineCp(right.mySplineCp){};
+          mySplineCp(right.mySplineCp),
+          myLinearT(right.myLinearT),
+          myLinearRho(right.myLinearRho),
+          myLinearCp(right.myLinearCp){};
 
     //! Assignment operator
     PengRobinsonMDGasPhase& operator=(const PengRobinsonMDGasPhase& right)
@@ -40,6 +61,9 @@ public:
         PengRobinsonGasPhase::operator=(right);
         mySplineRho = right.mySplineRho;
         mySplineCp = right.mySplineCp;
+        myLinearT = right.myLinearT;
+        myLinearRho = right.myLinearRho;
+        myLinearCp = right.myLinearCp;
         return *this;
     };
 
@@ -52,15 +76,37 @@ public:
         return cPengRobinsonMDGas;
     }
 
-    //! read in file and set up spline
+    //! read in file and set up spline interpolation
     //! HARD-CODED for single species (AR) right now
     void setSpline(std::string fileName);
 
+    //! read in file and set up linear interpolation
+    //! HARD-CODED for single species (AR) right now
+    void setLinear(std::string fileName);
+
+    //! pressure()
+    virtual doublereal pressure() const;
+
+    //! Set the pressure at constant temperature and composition.
+    virtual void setPressure(doublereal p);
+
+    //! Get cp_mole
+    virtual doublereal cp_mole() const;
+
 private:
+    // linear interpolation, used with setLinear()
+    double linearInterp(const std::vector<double>& T,
+                        const std::vector<double>& var, double Tnow) const;
+
     //! spline object
     tk::spline mySplineRho;
-    tk::spline mySplineCp;
+    tk::spline mySplineCp; // cp_mole
+
+    std::vector<double> myLinearT;
+    std::vector<double> myLinearRho;
+    std::vector<double> myLinearCp;
 };
+
 }
 
 #endif
--- src/thermo/PengRobinsonMDGasPhase.cpp
+++ src/thermo/PengRobinsonMDGasPhase.cpp
@@ -28,16 +28,112 @@ void PengRobinsonMDGasPhase::setSpline(std::string fileName)
     // infile.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
     double T_tmp, rho_tmp, cp_tmp;
+    int i = 1;
     while (infile >> T_tmp >> rho_tmp >> cp_tmp) {
+        if (i == 1) {
+            T_SPLINE.push_back(10.0);
+            rho_SPLINE.push_back(rho_tmp);
+            cp_SPLINE.push_back(cp_tmp * 4184e3);
+            T_SPLINE.push_back(T_tmp - 10.0);
+            rho_SPLINE.push_back(rho_tmp);
+            cp_SPLINE.push_back(cp_tmp * 4184e3);
+        }
         T_SPLINE.push_back(T_tmp);
         rho_SPLINE.push_back(rho_tmp);
-        cp_SPLINE.push_back(cp_tmp);
-        std::cout << T_tmp << rho_tmp << cp_tmp << std::endl;
+        cp_SPLINE.push_back(cp_tmp * 4184e3);
+
+        std::cout << i << ": " <<  T_tmp << " " << rho_tmp << " " << cp_tmp << std::endl;
+        i++;
     }
+    T_SPLINE.push_back(T_tmp + 10.0);
+    rho_SPLINE.push_back(rho_tmp);
+    cp_SPLINE.push_back(cp_tmp * 4184e3);
+    T_SPLINE.push_back(5000.0);
+    rho_SPLINE.push_back(rho_tmp);
+    cp_SPLINE.push_back(cp_tmp * 4184e3);
+
     infile.close();
 
     mySplineRho.set_points(T_SPLINE, rho_SPLINE);
     mySplineCp.set_points(T_SPLINE, cp_SPLINE);
+}
+
+void PengRobinsonMDGasPhase::setLinear(std::string fileName)
+{
+    myLinearT.clear();
+    myLinearRho.clear();
+    myLinearCp.clear();
+
+    std::ifstream infile(fileName);
+    if (!infile) {
+        std::cout << "couldn't open " << fileName << std::endl;
+        throw CanteraError("Error in PengRobinsonMDGasPhase.h",
+                           "read file for spline error...\n\n");
+    }
+    // infile.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    double T_tmp, rho_tmp, cp_tmp;
+    int i = 1;
+    while (infile >> T_tmp >> rho_tmp >> cp_tmp) {
+
+        myLinearT.push_back(T_tmp);
+        myLinearRho.push_back(rho_tmp);
+        myLinearCp.push_back(cp_tmp * 4184e3);
+
+        std::cout << i << ": " <<  T_tmp << " " << rho_tmp << " " << cp_tmp << std::endl;
+        i++;
+    }
+
+    infile.close();
+}
+
+double PengRobinsonMDGasPhase::linearInterp(const std::vector<double>& T,
+                                            const std::vector<double>& var,
+                                            double Tnow) const
+{
+    int N = T.size();
+
+    // no extrapolation
+    if (Tnow <= T[0]) return var[0];
+    if (Tnow >= T[N - 1]) return var[N - 1];
+
+    int i = 1;
+    while ((Tnow > T[i]) && (i < N - 1)) i++;
+    return var[i - 1] +
+           (var[i] - var[i - 1]) / (T[i] - T[i - 1]) * (Tnow - T[i - 1]);
+}
+
+doublereal PengRobinsonMDGasPhase::pressure() const { return 65.0 * OneAtm; }
+
+void PengRobinsonMDGasPhase::setPressure(doublereal p)
+{
+    // temperature is set already
+    PengRobinsonGasPhase::setPressure(p); // need this for partial h
+    //printf("%f: %f, %f\n", temperature(), density(), mySplineRho(temperature()));
+    //if (mySplineRho(temperature()) < 0) printf("%f\n", temperature());
+
+    //if (temperature() < 80) {
+    //    setDensity(1551.0);
+    //} else if (temperature() > 228) {
+    //    setDensity(154.0);
+    //} else {
+    //    setDensity(mySplineRho(temperature()));
+    //}
+
+    //setDensity(10.0);
+    //setDensity(mySplineRho(temperature()));
+    setDensity(linearInterp(myLinearT, myLinearRho, temperature()));
+}
+
+doublereal PengRobinsonMDGasPhase::cp_mole() const
+{
+    // temperature is set already
+    //return mySplineCp(temperature());
+    //printf("%g: %g, %g\n", temperature(), PengRobinsonGasPhase::cp_mole(), mySplineCp(temperature()));
+
+    //return PengRobinsonGasPhase::cp_mole();
+    //return 4.0e4;
+    return linearInterp(myLinearT, myLinearCp, temperature());
 }
 
 }
